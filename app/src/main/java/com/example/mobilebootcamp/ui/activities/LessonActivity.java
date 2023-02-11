211fbf20b1fe63a52feb6d49bf4ca391aa62c555
nadiahass <hassouninadia20@gmail.com>
2023-02-11 16:01:51 +0100

    lesson activity with its different fragment

--- app/src/main/java/com/example/mobilebootcamp/ui/activities/LessonActivity.java
+++ app/src/main/java/com/example/mobilebootcamp/ui/activities/LessonActivity.java
@@ -1,19 +1,71 @@
 package com.example.mobilebootcamp.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.mobilebootcamp.R;
+import com.example.mobilebootcamp.databinding.ActivityLessonBinding;
 import com.example.mobilebootcamp.models.Lesson;
+import com.example.mobilebootcamp.ui.fragments.JavaCodeFragment;
+import com.example.mobilebootcamp.ui.fragments.XmlCodeFragment;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class LessonActivity extends AppCompatActivity {
-
+    private ActivityLessonBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lesson);
+        binding = ActivityLessonBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         Lesson lesson = (Lesson) getIntent().getSerializableExtra("lesson");
+
+        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = new Fragment();
+                Bundle args = new Bundle();
+
+                switch (item.getItemId()){
+                    case R.id.java:
+                        selectedFragment = new JavaCodeFragment();
+                        args.putString("lesson" ,lesson.getJavaCode());
+                        break;
+
+                    case R.id.xml:
+                        selectedFragment = new XmlCodeFragment();
+                        args.putString("lesson" , lesson.getXmlCode());
+                        break;
+
+                    case R.id.result: {
+                        try {
+                            selectedFragment = (Fragment) (Class.forName("com.example.mobilebootcamp.ui.fragments.resultFragments." + lesson.getResultFragmentName()).newInstance());
+                        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                }
+                openSelectedFragment(selectedFragment , args);
+                return true;
+            }
+        });
+
+        if (savedInstanceState == null) {
+            binding.bottomNavigation.setSelectedItemId(R.id.java);
+        }
+    }
+
+    private void openSelectedFragment(Fragment selectedFragment, Bundle args) {
+        selectedFragment.setArguments(args);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
     }
 }
--- app/src/main/java/com/example/mobilebootcamp/ui/fragments/XmlCodeFragment.java
+++ app/src/main/java/com/example/mobilebootcamp/ui/fragments/XmlCodeFragment.java
@@ -2,6 +2,7 @@ package com.example.mobilebootcamp.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +10,27 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mobilebootcamp.R;
+import com.example.mobilebootcamp.databinding.FragmentXmlCodeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link XmlCodeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class XmlCodeFragment extends Fragment {
+    private FragmentXmlCodeBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @Override
+    public View onCreateView (@NonNull LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle savedInstanceState) {
+        binding = FragmentXmlCodeBinding.inflate(inflater, container, false);
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        String javaCode = getArguments().getString("lesson");
 
-    public XmlCodeFragment() {
-        // Required empty public constructor
-    }
+        binding.codeView.setCode(javaCode , "xml");
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment XmlCodeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static XmlCodeFragment newInstance(String param1, String param2) {
-        XmlCodeFragment fragment = new XmlCodeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_xml_code, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
