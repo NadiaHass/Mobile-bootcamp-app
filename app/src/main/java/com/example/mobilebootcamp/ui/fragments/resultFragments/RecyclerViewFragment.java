07c6e53cdacaaf00c382eaf1b8670e180e42fba3
nadiahass <hassouninadia20@gmail.com>
2023-02-13 20:56:02 +0100

    commit

--- app/src/main/java/com/example/mobilebootcamp/ui/fragments/resultFragments/RecyclerViewFragment.java
+++ app/src/main/java/com/example/mobilebootcamp/ui/fragments/resultFragments/RecyclerViewFragment.java
@@ -3,64 +3,44 @@ package com.example.mobilebootcamp.ui.fragments.resultFragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mobilebootcamp.R;
+import com.example.mobilebootcamp.adapters.LanguagesAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecyclerViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class RecyclerViewFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RecyclerViewFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecyclerViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecyclerViewFragment newInstance(String param1, String param2) {
-        RecyclerViewFragment fragment = new RecyclerViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+
+        ArrayList<String> languages = new ArrayList<>();
+        languages.add("Java");
+        languages.add("Kotlin");
+        languages.add("C++");
+        languages.add("Python");
+        languages.add("Ruby");
+        languages.add("C#");
+        languages.add("Php");
+        languages.add("Swift");
+        languages.add("Go");
+        languages.add("Pascal");
+        languages.add("Delphi");
+
+
+        RecyclerView recyclerView = view.findViewById(R.id.rv_languages);
+        LanguagesAdapter languagesAdapter = new LanguagesAdapter(languages);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity() , RecyclerView.VERTICAL , false));
+        recyclerView.setAdapter(languagesAdapter);
+        return view;
     }
 }
