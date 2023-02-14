392b1314797526f0ea1697db5d6d286b571cad11
nadiahass <hassouninadia20@gmail.com>
2023-02-14 11:24:02 +0100

    lifecycle and string.xml

--- app/src/main/java/com/example/mobilebootcamp/ui/fragments/resultFragments/FragmentsFragment.java
+++ app/src/main/java/com/example/mobilebootcamp/ui/fragments/resultFragments/FragmentsFragment.java
@@ -7,60 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.mobilebootcamp.R;
+import com.example.mobilebootcamp.ui.fragments.additional.Fragment1;
+import com.example.mobilebootcamp.ui.fragments.additional.Fragment2;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentsFragment newInstance(String param1, String param2) {
-        FragmentsFragment fragment = new FragmentsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragments, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_fragments, container, false);
+
+        Button button1 = view.findViewById(R.id.btn_fragment1);
+        Button button2 = view.findViewById(R.id.btn_fragment2);
+
+        button1.setOnClickListener(v -> {
+            Fragment fragment1 = new Fragment1();
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        });
+
+        button2.setOnClickListener(v -> {
+            Fragment fragment2 = new Fragment2();
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+
+        });
+
+        return view;
     }
 }
