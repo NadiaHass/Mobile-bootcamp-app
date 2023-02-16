3d4a1bce143444a83b2c604912886508cad2eebd
sara <bakiri.sara1@gmail.com>
2023-02-16 01:32:06 +0100

    alert dialog + option menu DONE

--- app/src/main/java/com/example/mobilebootcamp/ui/fragments/resultFragments/AlertDialogFragment.java
+++ app/src/main/java/com/example/mobilebootcamp/ui/fragments/resultFragments/AlertDialogFragment.java
@@ -1,5 +1,7 @@
 package com.example.mobilebootcamp.ui.fragments.resultFragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +9,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mobilebootcamp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlertDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AlertDialogFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AlertDialogFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlertDialogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlertDialogFragment newInstance(String param1, String param2) {
-        AlertDialogFragment fragment = new AlertDialogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private EditText name;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alert_dialog, container, false);
+        Button btn = view.findViewById(R.id.btn_click_me);
+        EditText name = view.findViewById(R.id.ed_name_alert_dialog);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+                dialog.setTitle("Welcome");
+                dialog.setMessage("Hello "+String.valueOf(name.getText())+" this is an alert dialog." +
+                        "\n Do you want to delete your name ?");
+                dialog.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        name.setText("");
+                    }
+                });
+                dialog.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alert_dialog, container, false);
+        return view;
     }
 }
