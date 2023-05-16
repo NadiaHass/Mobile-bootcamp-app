a464b004f0c906a88e909b38b6e7b3cf5efeabc4
nadiahass <hassouninadia20@gmail.com>
2023-05-16 14:56:08 +0100

    animation

--- app/src/main/java/com/example/mobilebootcamp/ui/activities/SplashActivity.java
+++ app/src/main/java/com/example/mobilebootcamp/ui/activities/SplashActivity.java
@@ -5,25 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.mobilebootcamp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        logo = findViewById(R.id.iv_logo);
         openModulesActivity();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        logo.startAnimation(AnimationUtils.loadAnimation(this ,
+                R.anim.left_to_right));
     }
 
     private void openModulesActivity() {
         Handler handler = new Handler();
         handler.postDelayed(() -> {
+
             Intent openMainActivity =  new Intent(SplashActivity.this, ModulesActivity.class);
             startActivity(openMainActivity);
+
             finish();
+
         }, 2000);
 
     }
